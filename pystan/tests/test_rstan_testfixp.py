83f56aa59528cca9929cc01058c8b910819c883c
Allen Riddell <abr@ariddell.org>
2014-08-05 13:56:57 -0400

    WIP reproduce fixedp bug

--- pystan/tests/test_rstan_testfixp.py
+++ pystan/tests/test_rstan_testfixp.py
@@ -17,5 +17,14 @@ class TestFixedParam(unittest.TestCase):
             y <- normal_rng(0, 1);
             }
             """
-        fit = stan(model_code=code)
-        fit2 = stan(fit=fit, algorithm='Fixed_param')
+        self.assertRaises(RuntimeError, pystan.stan, model_code=code)
+
+        fit = pystan.stan(model_code=code, iter=1000, chains=1, algorithm='Fixed_param')
+        extr = fit.extract()
+        self.assertTrue(-10 < np.mean(extr['y']) < 10)
+        self.assertTrue(0 < np.std(extr['y']) < 10)
+
+        fit2 = pystan.stan(fit=fit, iter=1000, chains=1, algorithm='Fixed_param')
+        extr = fit2.extract()
+        self.assertTrue(-10 < np.mean(extr['y']) < 10)
+        self.assertTrue(0 < np.std(extr['y']) < 10)
